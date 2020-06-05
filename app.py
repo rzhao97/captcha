601af7e4c110f4d3920d201ad04869f03dd045e7
Ubuntu <ubuntu@ip-172-31-29-176.us-west-1.compute.internal>
2020-06-05 20:38:51 +0000

    updated webapp

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import dash
+import dash_daq as daq
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
 
-import dash_daq as daq
 from dash_canvas import DashCanvas
+from dash.dependencies import Input, Output, State
 from dash_canvas.utils import array_to_data_url, parse_jsonstring
-from dash_table import DataTable
 
 
 import subprocess
@@ -21,78 +21,77 @@ import time
 import requests
 from skimage import io
 from tensorflow import keras
+import cv2
 import matplotlib.pyplot as plt
 
 from src.predict import *
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-columns = ['type', 'width', 'height', 'scaleX', 'strokeWidth', 'path']
-canvas_height = 50
-canvas_width = 200
-shape = (100,400)
+canvas_height = 200
+canvas_width = 800
+shape = (200,800)
 
 chars = string.ascii_lowercase + "0123456789"
 #model = keras.models.load_model('draw_model.h5')
+# -------------------------------------------------------------------- #
 
+# litera, flatly, mintly
+external_stylesheets =['https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/litera/bootstrap.min.css']+['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 # -------------------------------------------------------------------- #
 # -------------------------------------------------------------------- #
 
-app.layout = html.Div(id='dark', children=[
-    html.H1(children='Breaking Captcha', style={'text-align': 'center'}),
-    
-    daq.ToggleSwitch(
-        id='daq-light-dark-theme',
-        label=['Light', 'Dark'],
-        style={'width': '250px', 'margin': 'auto'}, 
-        value=False
-    ),
-    
-    html.Br(), 
-    
+app.layout = html.Div(children=[
+    html.H1(children='Breaking CAPTCHA', 
+            style={'text-align': 'center', 'font-size':'70px',
+                    #'color': 'darkcyan',
+                    'background': '#60A0B5',
+                    #'border': '3px solid indigo',
+                    'padding': '7px',
+                    #font-family: monospace;
+                  }),
+        
+    # Example captcha images
     html.Img(src=app.get_asset_url('2a76a.png'), style={'width': '250px'}),
     html.Img(src=app.get_asset_url('exd3k.png'), style={'width': '250px'}),
     html.Img(src=app.get_asset_url('ky5fm.png'), style={'width': '250px'}),
     html.Img(src=app.get_asset_url('w8bp5.png'), style={'width': '250px'}),
+    
+    #html.Br(),
 
-    #html.Div([
-    html.H6('Draw a 5 Character Captcha and press Save to see work'),
-    #html.Div([
-    DashCanvas(id='canvas',
-               lineWidth=8,
-               hide_buttons=["zoom", "pan", "line", "pencil", "rectangle", "select"],
-               lineColor='black',
-               height=100,
-               width=400,
-               ),
-    #], className="five columns"),
+    # Dash canvas for drawing captcha
+    html.Div([
+            html.H2('Draw a CAPTCHA and press Save to see work'),
+            DashCanvas(id='canvas',
+                       lineWidth=17,
+                       lineColor='black',
+                       hide_buttons=["zoom", "pan", "line", "pencil", 
+                                     "rectangle", "select"],
+                       height=200,
+                       width=800,
+                       ),
+    ], style={'display': 'inline-block', 'vertical-align': 'middle'}),
     
-    html.Img(id='captcha_img'),# height=100, width=400),
-    #]), className="five columns"),
-    #]),
-    
-    DataTable(id='canvas-table',
-              style_cell={'textAlign': 'left',
-                         'display': 'none'
-                         },
-              columns=[{"name": i, "id": i} for i in columns]),
+    # Image of drawn captcha
+    html.Img(id='captcha_img', style={"width":"50%", "height":"50%"}),
     
     # Hidden div inside the app that stores the drawn image
     html.Div(id='drawn_img', style={'display': 'none'}),
     
-    html.Div([
-        html.H4('Run Captcha Breaking Model'),
-            html.Button('BREAK', id='start', n_clicks=0)
-    ]),
-    html.Div([
-        html.H4(id='prediction')
-    ]),
+    html.Br(),
     
-    
-])
+    # Break button and loading spinner that becomes prediction
+    html.Div([
+            html.H5('Run Captcha Breaking Model'),
+            dbc.Button('BREAK', id='start', size='lg', block=True, color='info', 
+                       n_clicks=0, style={'display':'inline-block', "width": "30rem",
+                                          "height":"6rem", 'font-size':'20px'}),
+            html.Br(),
+            dbc.Spinner(html.Div([html.H1(id='prediction')]),
+                       spinner_style={"width":"7rem", "height":"7rem", "color":"info"}),
+    ]),
+
+], style={'textAlign': 'center'})
 
 
 # -------------------------------------------------------------------- #
@@ -106,19 +105,12 @@ def show_draw(string):
         raise PreventUpdate
         
     mask = parse_jsonstring(string, shape)
+    mask = (255 * mask).astype(np.uint8)
+    mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     
-    captcha_img = array_to_data_url((255 * mask).astype(np.uint8))
+    captcha_img = array_to_data_url(mask)
     
     return captcha_img, string
-
-@app.callback(Output('canvas-table', 'data'),
-              [Input('canvas', 'json_data')])
-def update_data(string):
-    if string:
-        data = json.loads(string)
-    else:
-        raise PreventUpdate
-    return data['objects'][1:]
 
 
 @app.callback(
@@ -126,12 +118,12 @@ def update_data(string):
      #Output('drawn_img', 'children')],
     [
      Input('start', 'n_clicks'),
-     Input('drawn_img', 'children')
     ],
+    [State('drawn_img', 'children')],
 )
 def predict(on, string):
     
-    if not on:
+    if not on or not string:
         raise PreventUpdate
     
     print('sending string with length:',len(string))
@@ -144,36 +136,14 @@ def predict(on, string):
         outputs.append(line)
         print(line)
         
-    print('Call Completed...')    
-    pred = str(outputs[-1])[2:-3]
-
-    return pred, None
-
-    """mask = parse_jsonstring(string, shape)
-    img = (255 * mask).astype(np.uint8)
-    #print(type(img))
-
-    print('About to predict img...')
-    split = split_drawn(img)
-    pred = model.predict(split)
-    #pred = predict_drawn(model, img)
-    print('Success')
-    #pred = 'abcd'
+    print('Prediction Completed...')    
     
-    return str(pred.shape) # str(pred)"""
+    
+    pred = str(outputs[-1])[2:-3]
+    pred = 'Predicted Text: ' + pred
 
-#-----------------------------------------------------------------
-# Dark Theme Callbacks
+    return pred
 
-@app.callback(
-    Output('dark', 'style'),
-    [Input('daq-light-dark-theme', 'value')]
-)
-def change_bg(dark_theme):
-    if(dark_theme):
-        return {'background-color': '#303030', 'color': 'white'}
-    else:
-        return {'background-color': 'white', 'color': 'black'}
 
 # -------------------------------------------------------------------- #
 # -------------------------------------------------------------------- #
--- receiver.py
+++ receiver.py
@@ -1,12 +1,13 @@
 import sys
 from time import sleep
 
-from tensorflow import keras
 from dash_canvas.utils import array_to_data_url, parse_jsonstring
+from tensorflow import keras
+import cv2
 
 from src.predict import *
 
-shape = (100,400)
+shape = (200,800)
 chars = '0123456789' + string.ascii_lowercase
 model = keras.models.load_model('draw_model.h5')
 
@@ -17,6 +18,7 @@ string = sys.argv[1]
 
 mask = parse_jsonstring(string, shape)
 img = (255 * mask).astype(np.uint8)
+img = cv2.resize(img, (200,50), interpolation = cv2.INTER_AREA)
     
 # split the drawn image
 split_img = split_drawn(img)
@@ -31,4 +33,9 @@ for i in pred_arr:
     
 print(str(img.shape))
 print(str(split_img.shape))
+
+"""for i in split_img:
+    char_img = array_to_data_url((i.reshape(28,28).astype(np.uint8)))
+    print(char_img)"""
+
 print(pred)
