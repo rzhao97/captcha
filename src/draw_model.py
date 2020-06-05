c5611a7748f032bd1ce17936f2d912d77e453362
Ubuntu <ubuntu@ip-172-31-29-176.us-west-1.compute.internal>
2020-06-05 02:49:24 +0000

    handwritten model

---
+++ src/draw_model.py
@@ -0,0 +1,93 @@
+import os
+import string
+import random
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras import Model
+
+from PIL import Image
+import cv2
+
+from emnist import list_datasets
+from emnist import extract_training_samples, extract_test_samples
+
+
+def remove_upper(X_old, labels):
+    nums = list(range(0, 10))
+    upper = list(range(10, 36))
+    lower = list(range(36, 62))
+    X = []
+    y = []
+    for x, lab in zip(X_old, labels):
+        if lab in nums:
+            X.append(x)
+            y.append(lab)
+        elif lab in upper:
+            continue
+        elif lab in lower:
+            lab -= 26
+            X.append(x)
+            y.append(lab)
+    
+    return np.array(X), np.array(y)
+
+def load_data():
+    X_train, train_labels = extract_training_samples('byclass')
+    X_test, test_labels = extract_test_samples('byclass')
+    
+    X_train, train_labels = remove_upper(X_train, train_labels)
+    X_test, test_labels = remove_upper(X_test, test_labels)
+    
+    chars = '0123456789' + string.ascii_lowercase
+    num_chars = len(chars)
+    
+    X_train = X_train.reshape(-1, 28, 28, 1)
+    X_test = X_test.reshape(-1, 28, 28, 1)
+        
+    return X_train, X_test, train_labels, test_labels
+
+def build_model():
+    
+    X_train, X_test, train_labels, test_labels = load_data()
+    
+    chars = '0123456789' + string.ascii_lowercase
+    num_chars = len(chars)
+    
+    input_shape = (28, 28, 1)
+
+    # Input layer
+    imgm = Input(shape=input_shape)
+
+    # Convolution and Max Pooling layers
+    cn1 = Conv2D(16, (5,5), padding='same', activation='relu')(imgm)
+    mp1 = MaxPooling2D(padding='same')(cn1)  
+
+    cn2 = Conv2D(32, (5,5), padding='same', activation='relu')(mp1)
+    mp2 = MaxPooling2D(padding='same')(cn2)  
+
+    cn3 = Conv2D(16, (5,5), padding='same', activation='relu')(mp2)
+    mp3 = MaxPooling2D(padding='same')(cn3)   
+
+    # Flatten and Dense layers
+    flat = Flatten()(mp3)
+    dens1 = Dense(128, activation='relu')(flat)
+    #drop = Dropout(0.5)(dens1)
+    bn = BatchNormalization()(dens1)
+    output = Dense(num_chars, activation='softmax')(bn)#(drop)
+
+    # Compile model
+    model = Model(imgm, output)
+    model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
+    
+    # Train model
+    history = model.fit(X_train, train_labels, batch_size=256, epochs=10, verbose=2, validation_split=0.2)
+    
+    
+    
+    return model
+    
---
+++ src/predict.py
@@ -0,0 +1,68 @@
+import os
+import string
+import random
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from PIL import Image
+import cv2
+
+# function to split handwritten captcha
+def split_drawn(drawn_img):
+    thresh = drawn_img.copy()
+
+    # find the contours (continuous blobs of pixels) the image
+    contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    letter_image_regions = []
+
+    # Iterate through the contours
+    for contour in contours:
+        # Find location of each character
+        (x, y, w, h) = cv2.boundingRect(contour)
+
+        # If width/height is too large to be a one character, split it
+        if w / h > 1.25:
+            half_width = int(w / 2)
+            letter_image_regions.append((x, y, half_width, h))
+            letter_image_regions.append((x + half_width, y, half_width, h))
+        else:
+            letter_image_regions.append((x, y, w, h))
+
+    # Sort the characters
+    letter_image_regions = sorted(letter_image_regions, key=lambda x: x[0])
+
+    # Add each resized character to an array
+    char_lst = []
+    for i, letter_bounding_box in enumerate(letter_image_regions):
+        # Get location of character
+        x, y, w, h = letter_bounding_box
+
+        # Extract the character
+        letter_image = thresh[y - 2:y + h + 2, x - 2:x + w + 2]
+        
+        # Resize the character
+        resized = cv2.resize(letter_image, (28,28), interpolation = cv2.INTER_AREA)
+        resized = resized.reshape(28,28,1)
+
+        char_lst.append(resized)
+    
+    return np.array(char_lst)
+
+def predict_drawn(model, img):
+    chars = '0123456789' + string.ascii_lowercase
+    
+    # split the drawn image
+    split_img = split_drawn(img)
+    
+    # predict each character
+    pred_arr = model.predict(split_img)
+    
+    pred = ''
+    for i in pred_arr:
+        c = chars[np.argmax(i)]
+        pred += c
+
+    return pred
