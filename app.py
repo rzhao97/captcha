50e5497797adeb02b5422fea693ab4664d938353
Ubuntu <ubuntu@ip-172-31-29-176.us-west-1.compute.internal>
2020-06-08 19:11:05 +0000

    webapp py file updated with style.css

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ import dash_daq as daq
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
 
 from dash_canvas import DashCanvas
-from dash.dependencies import Input, Output, State
 from dash_canvas.utils import array_to_data_url, parse_jsonstring
-
-
-import subprocess
 from dash.exceptions import PreventUpdate
+import subprocess
 
 import numpy as np
 import string
@@ -22,32 +20,32 @@ import requests
 from skimage import io
 from tensorflow import keras
 import cv2
-import matplotlib.pyplot as plt
-
-from src.predict import *
 
 canvas_height = 200
 canvas_width = 800
 shape = (200,800)
 
 chars = string.ascii_lowercase + "0123456789"
-#model = keras.models.load_model('draw_model.h5')
 # -------------------------------------------------------------------- #
 
 # litera, flatly, mintly
-external_stylesheets =['https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/litera/bootstrap.min.css']+['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootswatch/4.5.0/litera/bootstrap.min.css'] +['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+
+# -------------------------------------------------------------------- #
+# -------------------------------------------------------------------- #
+
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-# -------------------------------------------------------------------- #
-# -------------------------------------------------------------------- #
+app.head = [html.Link(rel='stylesheet', href='style.css')]
 
-app.layout = html.Div(children=[
+app.layout = html.Div(id='grad1', children=[
     html.H1(children='Breaking CAPTCHA', 
             style={'text-align': 'center', 'font-size':'70px',
-                    #'color': 'darkcyan',
-                    'background': '#60A0B5',
+                    'color': 'black',
+                    #'background': '#60A0B5',
                     #'border': '3px solid indigo',
-                    'padding': '7px',
+                    #'padding': '7px',
                     #font-family: monospace;
                   }),
         
@@ -57,12 +55,13 @@ app.layout = html.Div(children=[
     html.Img(src=app.get_asset_url('ky5fm.png'), style={'width': '250px'}),
     html.Img(src=app.get_asset_url('w8bp5.png'), style={'width': '250px'}),
     
-    #html.Br(),
+    html.Br(),
+    html.Br(),    
 
     # Dash canvas for drawing captcha
-    html.Div([
-            html.H2('Draw a CAPTCHA and press Save to see work'),
-            DashCanvas(id='canvas',
+    #html.Div([html.H2('Draw a CAPTCHA and press Save to see work')]),
+    html.Div([html.H2('Draw a CAPTCHA and press Save to see work'),
+              DashCanvas(id='canvas',
                        lineWidth=12,
                        lineColor='black',
                        hide_buttons=["zoom", "pan", "line", "pencil", 
@@ -70,19 +69,23 @@ app.layout = html.Div(children=[
                        height=200,
                        width=800,
                        ),
-    ], style={'display': 'inline-block', 'vertical-align': 'middle'}),
+    ], style={'display': 'inline-block', 'vertical-align': 'middle',
+              'background': '#60A0B5', 'border': '3px solid indigo', 'padding': '2px',}),
+    
+    html.Br(),
+    html.Br(),
     
     # Image of drawn captcha
-    html.Img(id='captcha_img', style={"width":"50%", "height":"50%"}),
+    html.Div([html.Img(id='captcha_img')]),
     
     # Hidden div inside the app that stores the drawn image
     html.Div(id='drawn_img', style={'display': 'none'}),
     
-    html.Br(),
+    #html.Br(),
     
     # Break button and loading spinner that becomes prediction
     html.Div([
-            html.H5('Run Captcha Breaking Model'),
+            html.H5('Run CAPTCHA Breaking Model'),
             dbc.Button('BREAK', id='start', size='lg', block=True, color='info', 
                        n_clicks=0, style={'display':'inline-block', "width": "30rem",
                                           "height":"6rem", 'font-size':'20px'}),
@@ -93,9 +96,7 @@ app.layout = html.Div(children=[
     ]),
     
     #html.Img(src=app.get_asset_url('robots.jpg'), style={'width': '250px','display': 'flex'}),
-
-
-], style={'textAlign': 'center'})
+])
 
 
 # -------------------------------------------------------------------- #
@@ -107,13 +108,16 @@ app.layout = html.Div(children=[
 def show_draw(string):
     if not string:
         raise PreventUpdate
-        
+    
+    # Convert image string to displayable image 
     mask = parse_jsonstring(string, shape)
     mask = (255 * mask).astype(np.uint8)
+    mask = cv2.resize(mask, (400,100), interpolation = cv2.INTER_AREA)
     mask = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     
     captcha_img = array_to_data_url(mask)
     
+    # Show image and save image string
     return captcha_img, string
 
 
@@ -127,14 +131,17 @@ def show_draw(string):
 )
 def predict(on, string):
     
+    # Only predict if something is drawn and BREAK is pushed
     if not on or not string:
         raise PreventUpdate
     
     print('sending string with length:',len(string))
     
+    # Send image as string to receiver.py to predict with model 
     cmd = ['python', 'receiver.py'] + [string]
     p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     
+    # Receive outputs from receiver.py
     outputs = []
     for line in p.stdout.readlines():
         outputs.append(line)
@@ -142,7 +149,7 @@ def predict(on, string):
         
     print('Prediction Completed...')    
     
-    
+    # Find prediction to print out
     pred = str(outputs[-1])[2:-3]
     pred = 'Predicted Text: ' + pred
 
--- receiver.py
+++ receiver.py
@@ -1,36 +1,41 @@
-import sys
-from time import sleep
-
-from dash_canvas.utils import array_to_data_url, parse_jsonstring
 from tensorflow import keras
 import cv2
+import sys
+import string
+import numpy as np
 
-from src.predict import *
+from dash_canvas.utils import array_to_data_url, parse_jsonstring
+
+#from src.predict import *
 
 shape = (200,800)
 chars = '0123456789' + string.ascii_lowercase
 
-#model = keras.models.load_model('draw_model.h5')
-model = keras.models.load_model('full_draw_model1.h5')
+#model = keras.models.load_model('models/draw_model.h5')
+model = keras.models.load_model('models/final_draw_model.h5')
 
 print('receiving string with length:', len(sys.argv[1]))
 
+# Get image string from app.py
 string = sys.argv[1]
 
+# Convert image string to numpy array
 mask = parse_jsonstring(string, shape)
 img = (255 * mask).astype(np.uint8)
 img = cv2.resize(img, (200,50), interpolation = cv2.INTER_AREA)
  
 
-# for with line
+# Use model to predict image
 img = img / 255.0
 onehotpred = np.array(model.predict(img.reshape(1,50,200,1))).reshape(5,36)    
 pred = ''
-    
+
+# Find prediction for each character output
 for i in onehotpred:
     c = chars[np.argmax(i)]
     pred += c
-    
+
+# Return prediction on app.py
 print(pred)
  
     
